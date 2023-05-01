3bf5891c1653436c658025f14cb05505513c40c7
Jonas <jonaso@web.de>
2023-05-01 13:17:14 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import { defineConfig } from 'vite'
 export default defineConfig(({ command, mode }) => {
   return {
     root: './src',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    },
+    plugins: [],
+    publicDir: '../public',
     build: {
       outDir: '../dist',
       minify: false,
